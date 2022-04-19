7cd01a2770ec00a11553f8b39557bf1e6784534c
Klaus Ferreira <claudenir.ferreira@yandex.com>
2022-04-19 15:30:40 -0300

    Update Discord link

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function IndexPage() {
 
 				<p>DevCaminhante é um projeto dedicado ao acolhimento tanto para quem está pensando em entrar na área de programação como para quem está começando a aprender nesta área. Também recebe de braços abertos os devs mais experientes que querem compartilhar conhecimento e também aprender, estimulando o crescimento mútuo, sempre com a mentalidade que todos somos aprendizes. :)</p>
 
-				<h2>ATENÇÃO: Nosso principal canal de comunicação é pelo endereço <a href="https://t.me/DevCaminhante">https://t.me/DevCaminhante</a>. Caso ocorra um bloqueio do Telegram, a comunidade continua se reunindo no Discord, pelo endereço <a href="https://discord.gg/jqwha8GG">https://discord.gg/jqwha8GG</a> e seguindo as mesmas regras descritas no endereço <a href="https://github.com/DevCaminhante/docs/blob/dev/regras.md">https://github.com/DevCaminhante/docs/blob/dev/regras.md</a>. Quaisquer atualizações serão incluídas aqui nessa seção, na página inicial. Obrigado pela compreensão.</h2>
+				<h2>ATENÇÃO: Nosso principal canal de comunicação é pelo endereço <a href="https://t.me/DevCaminhante">https://t.me/DevCaminhante</a>. Caso ocorra um bloqueio do Telegram, a comunidade continua se reunindo no Discord, pelo endereço <a href="https://discord.gg/RZBCb4cGJR">https://discord.gg/RZBCb4cGJR</a> e seguindo as mesmas regras descritas no endereço <a href="https://github.com/DevCaminhante/docs/blob/dev/regras.md">https://github.com/DevCaminhante/docs/blob/dev/regras.md</a>. Quaisquer atualizações serão incluídas aqui nessa seção, na página inicial. Obrigado pela compreensão.</h2>
 			</div>
 	)
 }
